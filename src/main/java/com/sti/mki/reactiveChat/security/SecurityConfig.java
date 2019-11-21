0ef017791451872dad2e50630b5efd99c35b8186
s.titov <stitov@qulix.com>
2019-11-21 18:22:25 +0300

    EnableWebFluxSecurity

--- src/main/java/com/sti/mki/reactiveChat/security/SecurityConfig.java
+++ src/main/java/com/sti/mki/reactiveChat/security/SecurityConfig.java
@@ -1,19 +1,8 @@
 package com.sti.mki.reactiveChat.security;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebFluxSecurity
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .permitAll()
-                .and().csrf().disable();
-    }
 }
