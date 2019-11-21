028e12eba6959d580d1966ceb93cb01908b5ae57
s.titov <stitov@qulix.com>
2019-11-21 19:02:25 +0300

    added login & logout form

--- src/main/java/com/sti/mki/reactiveChat/security/SecurityConfig.java
+++ src/main/java/com/sti/mki/reactiveChat/security/SecurityConfig.java
@@ -1,8 +1,38 @@
 package com.sti.mki.reactiveChat.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .httpBasic().and()
+                .formLogin();
+        return http.build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User
+                .withUsername("user")
+                .password(encoder.encode("password"))
+                .roles("USER")
+                .build();
+        return new MapReactiveUserDetailsService(user);
+    }
 
 }
